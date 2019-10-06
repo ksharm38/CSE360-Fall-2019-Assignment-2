4a7cc67b59a00b485dcbae1f83924890c99d9114
ksharm38 <kshrm38@asu.edu>
2019-10-05 22:32:02 -0700

    First Version

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,53 @@
+/*
+Name : Kunal Sharma
+CSE 360 Assignment - 2
+*/
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+        /*
+        This is constructor of class AddingMachine
+        */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+        /*
+        getTotal Function provides total value after operations.
+        */
+	public int getTotal () {
+		return 0; // returns total value after operatons are performed.
+	}
+        
+	/*
+        add function performs addtion.
+        */
+	public void add (int value) {
+		                     // adds value to the total value
+	}
+        
+	/*
+        subtract function performs subtraction
+        */
+        
+	public void subtract (int value) {
+		// subtracts value from the total value.
+	}
+        
+	/*
+        toString function keeps track of transction. 
+        */	
+	public String toString () {
+		return ""; // returns history of the transactions in the form of string.
+	}
+
+        /*
+        clear function clears total value and transaction history.
+        */
+	public void clear() {
+	    // clears total value and history of transactions.
+	}
+}
