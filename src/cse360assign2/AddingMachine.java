dd42040d3379b144cc1dd97f5f29b802fa0f98c3
ksharm38 <kshrm38@asu.edu>
2019-10-09 22:30:02 -0700

    Update AddMachineFile - JavaDoc Added

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -4,28 +4,41 @@ CSE 360 Assignment - 2
 */
 package cse360assign2;
 
+/**
+ *
+ * @Kunal Sharma
+ */
 public class AddingMachine {
 
-	private int total;
-        private String transaction;
+    /**
+     * Stores total value after operations.
+     */
+    private int total;
+
+    /**
+     * Stores transaction history
+     */
+    private String transaction;
 	
-        /*
-        This is constructor of class AddingMachine
+        /**
+        *This is constructor of class AddingMachine
         */
 	public AddingMachine () {
 		total = 0;  // initialise total to 0
                 transaction = String.valueOf(total); // initialise string with "0"
 	}
 	
-        /*
-        getTotal Function provides total value after operations.
+        /**
+        *getTotal Function provides total value after operations.
+     * @return  integer
         */
 	public int getTotal () {
 		return total; // returns total value after operatons are performed.
 	}
         
-	/*
-        add function performs addtion.
+	/**
+        *add function performs addition.
+     * @param value integer
         */
 	public void add (int value) {
             
@@ -33,8 +46,9 @@ public class AddingMachine {
          transaction = transaction + " + " + value;
 	}
         
-	/*
-        subtract function performs subtraction
+	/**
+        *subtract function performs subtraction
+     * @param value integer
         */
         
 	public void subtract (int value) {
@@ -42,16 +56,19 @@ public class AddingMachine {
                 transaction = transaction + " - " + value;
         }
         
-	/*
-        toString function keeps track of transction. 
+	/**
+        *toString function keeps track of transaction.
+        * @return  integer
         */	
 	public String toString () {
 		return transaction; // returns history of the transactions in the form of string.
 	}
 
-        /*
-        clear function clears total value and transaction history.
-        */
+
+    /**
+     *clear function clears total value and transaction history.
+     */
+
 	public void clear() {
 	    total = 0;
             transaction = "0";// clears total value and history of transactions.
